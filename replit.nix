f3fc93d55f00b790b1846d00afda4f7ba219bc34
Spielcrypto <spieljs@gmail.com>
2024-12-23 20:50:11 +0100

    fix deployment

--- replit.nix
+++ replit.nix
@@ -1,7 +1,12 @@
 { pkgs }: {
     deps = [
+      pkgs.mongosh
         pkgs.openssh
         pkgs.cowsay
         pkgs.docker-compose
+        pkgs.docker
+        pkgs.redis
+        pkgs.mongodb
+        pkgs.certbot
     ];
 }
