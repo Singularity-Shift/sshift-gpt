138a9d9e28d1ca6d06cb53add7c430f86dd77b83
Spielcrypto <spieljs@gmail.com>
2025-04-12 22:11:48 +0500

    remove not existing action

--- tools-api/src/aptos-onchain/aptos-onchain.controller.ts
+++ tools-api/src/aptos-onchain/aptos-onchain.controller.ts
@@ -32,7 +32,6 @@ export class AptosOnchainController {
       'joule_get_user_position',
       'joule_get_user_all_positions',
       'emojicoin_get_market',
-      'panora_aggregator_price',
       'panora_aggregator_list',
     ];
 
